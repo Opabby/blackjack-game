807be16ff82d78af09287cd7bac55f01ec333af5
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-13 18:01:08 -0300

    Improving JS

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-let firstCard = 11;
+let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?");
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-    console.log("Congratulations, you won!");
+    message = "Congratulations, you won!";
+    hasBlackJack = true;
 } else {
-    console.log("aaaaaw, you lost :/");
+    message = "You're out of the game!";
+    isAlive = false;
 }
+
+console.log(message);
