56492ef861c6e222747801825ca644cc70c886db
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-17 15:20:53 -0300

    Adding getRandomCard()

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 7;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -8,6 +8,17 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10
+    } else if(randomNumber===1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
     renderGame();
@@ -32,7 +43,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 3;
+    let card = getRandomCard();
     sum += card;
     cards.push(card);
     renderGame();
